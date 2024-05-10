d9c3e4819688926f9061f4fe7095fec2ae508eaa
Gustavo <gustavo_schul@hotmail.com>
2024-05-10 15:31:19 -0300

    passar objeto Projects para index

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from portfolio.models import Projects
 
 def index(request):
-    return render(request, "portfolio/index.html")
+    projects = Projects.objects.order_by("-date")
+
+    return render(request, "portfolio/index.html", {'projects': projects})
