c8fc7011911b1107341fa89c3a3544a6e8fc7d8b
Gustavo <gustavo_schul@hotmail.com>
2024-06-04 21:52:06 -0300

    mudança para banco de dados local e arquivos estaticos e de midia locais

--- setup/settings.py
+++ setup/settings.py
@@ -170,14 +170,15 @@ if IS_HEROKU_APP:
 else:
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.1/howto/static-files/
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 
     # Media:
-    MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
     MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
