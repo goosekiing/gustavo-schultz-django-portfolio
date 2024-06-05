68c2e69d8d92ecb6a5f47f3c6dce0c9849b06a54
Gustavo <gustavo_schul@hotmail.com>
2024-06-04 21:51:48 -0300

    mudança para banco de dados local e arquivos estaticos e de midia locais

--- setup/settings.py
+++ setup/settings.py
@@ -96,14 +96,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASE_URL = str(os.getenv('DATABASE_URL'))
-DB_PASSWORD = str(os.getenv('COCKROACHLABS_DB_PASSWORD'))
-DATABASE_URL = DATABASE_URL.replace('@', f'{DB_PASSWORD}@')
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, engine='django_cockroachdb')
-}
-DATABASES['default']['OPTIONS'] = {
-    'sslrootcert': r'C:\Users\Gustavo S Schultz\AppData\Roaming\postgresql\root.crt',
+if IS_HEROKU_APP:
+    DATABASE_URL = str(os.getenv('DATABASE_URL'))
+    DB_PASSWORD = str(os.getenv('COCKROACHLABS_DB_PASSWORD'))
+    DATABASE_URL = DATABASE_URL.replace('@', f'{DB_PASSWORD}@')
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, engine='django_cockroachdb')
+    }
+    DATABASES['default']['OPTIONS'] = {
+        'sslrootcert': r'C:\Users\Gustavo S Schultz\AppData\Roaming\postgresql\root.crt',
+    }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
@@ -135,29 +143,41 @@ USE_I18N = True
 
 USE_TZ = True
 
-# AWS Configuration:
-AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
-AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
-AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.sa-east-1.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400'
-}
-AWS_QUERYSTRING_AUTH = False
-AWS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-}
+if IS_HEROKU_APP:
+    # AWS Configuration:
+    AWS_ACCESS_KEY_ID = str(os.getenv('AWS_ACCESS_KEY_ID'))
+    AWS_SECRET_ACCESS_KEY = str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+    AWS_STORAGE_BUCKET_NAME = str(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.sa-east-1.amazonaws.com'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400'
+    }
+    AWS_QUERYSTRING_AUTH = False
+    AWS_HEADERS = {
+        'Access-Control-Allow-Origin': '*',
+    }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_STORAGE = 'setup.custom_storages.StaticStorage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/4.1/howto/static-files/
+    STATICFILES_STORAGE = 'setup.custom_storages.StaticStorage'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Media files
-DEFAULT_FILE_STORAGE = 'setup.custom_storages.MediaStorage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+    # Media files
+    DEFAULT_FILE_STORAGE = 'setup.custom_storages.MediaStorage'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/4.1/howto/static-files/
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+
+    # Media:
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+    MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
