c651979b53c545f287551e532878a8deb78e8147
Gustavo <gustavo_schul@hotmail.com>
2024-06-10 14:19:48 -0300

    alterada definicao da linguagem do codigo para ingles

--- setup/settings.py
+++ setup/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
